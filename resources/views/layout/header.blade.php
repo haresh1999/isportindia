78f39358d4b10793bb2049f127c92f2ca8d92814
Haresh Chauhan <hareshc1999@gmail.com>
2023-07-01 18:12:07 +0530

    update

---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,175 @@
+<div class="container">
+    <nav class="style_navbar__r9UeE align-items-center position-sticky top-0 navbar navbar-expand navbar-light">
+      <a class="style_logo__iDBP2 navbar-brand d-block m-0 p-0" href="/"><span style="
+              display: block;
+              overflow: hidden;
+              width: initial;
+              height: initial;
+              background: none;
+              opacity: 1;
+              border: 0;
+              margin: 0;
+              padding: 0;
+              position: relative;
+            "><span style="
+                display: block;
+                width: initial;
+                height: initial;
+                background: none;
+                opacity: 1;
+                border: 0;
+                margin: 0;
+                padding: 0;
+                padding-top: 15.68702290076336%;
+              "></span><img alt="logo" src="/_next/static/media/logo.9bce43e9.svg" decoding="async"
+            data-nimg="responsive" style="
+                position: absolute;
+                top: 0;
+                left: 0;
+                bottom: 0;
+                right: 0;
+
+                padding: 0;
+                border: none;
+                margin: auto;
+                display: block;
+                width: 0;
+                height: 0;
+                min-width: 100%;
+                max-width: 100%;
+                min-height: 100%;
+                max-height: 100%;
+              " sizes="100vw" /></span></a><span
+        class="style_separator__n6bds d-none d-md-block ms-1 ms-md-2 ms-lg-3"></span>
+      <div class="style_langMenu__nnhmv common-dropdown d-none d-md-block dropdown">
+        <button id="language" aria-expanded="false" type="button" class="dropdown-toggle btn btn-link">
+          English
+        </button>
+      </div>
+      <div class="style_nav__1OO3C ms-auto align-items-center navbar-nav">
+        <a href="/sign-in/" class="d-none">Sign in</a>
+        <!-- <div class="">
+                        <div
+                            class="form_formSwitch__8A_aR style_modeSwitch__SPnb9 undefined d-flex align-items-center">
+                            <div class="d-none form-switch">
+                                <input type="checkbox" id="modeSwitch" class="form-check-input" />
+                            </div>
+                            <label for="modeSwitch" class="style_darkModeIcon__uOVf_ me-2"><span style="
+                    display: inline-block;
+                    overflow: hidden;
+                    width: initial;
+                    height: initial;
+                    background: none;
+                    opacity: 1;
+                    border: 0px;
+                    margin: 0px;
+                    padding: 0px;
+                    position: relative;
+                    max-width: 100%;
+                  "><span style="
+                      display: block;
+                      width: initial;
+                      height: initial;
+                      background: none;
+                      opacity: 1;
+                      border: 0px;
+                      margin: 0px;
+                      padding: 0px;
+                      max-width: 100%;
+                    "><img alt="" aria-hidden="true"
+                                            src="data:image/svg+xml,%3csvg%20xmlns=%27http://www.w3.org/2000/svg%27%20version=%271.1%27%20width=%2722%27%20height=%2722%27/%3e"
+                                            style="
+                        display: block;
+                        max-width: 100%;
+                        width: initial;
+                        height: initial;
+                        background: none;
+                        opacity: 1;
+                        border: 0px;
+                        margin: 0px;
+                        padding: 0px;
+                      " /></span><img alt="dark mode" src="/_next/static/media/light-mode.6f89d25e.svg"
+                                        decoding="async" data-nimg="intrinsic" class="style_lightModeImg__2_YH4"
+                                        style="
+                      position: absolute;
+                      inset: 0px;
+
+                      padding: 0px;
+                      border: none;
+                      margin: auto;
+                      display: block;
+                      width: 0px;
+                      height: 0px;
+                      min-width: 100%;
+                      max-width: 100%;
+                      min-height: 100%;
+                      max-height: 100%;
+                    " srcset="
+                      /_next/static/media/light-mode.6f89d25e.svg 1x,
+                      /_next/static/media/light-mode.6f89d25e.svg 2x
+                    " /></span></label>
+                        </div>
+                    </div> -->
+        <a class="style_navLink__aDOS0 style_fillBtn__xma7g rounded-pill" href="/sign-in/">Sign In</a>
+        <div class="hd-search-ot d-none d-md-block">
+          <div class="style_navLink__aDOS0 style_iconItem__NTDWb p-0 searchItem">
+            <button type="button" class="searchBtn border-0 btn btn-link">
+              <span style="
+                    display: block;
+                    overflow: hidden;
+                    width: initial;
+                    height: initial;
+                    background: none;
+                    opacity: 1;
+                    border: 0px;
+                    margin: 0px;
+                    padding: 0px;
+                    position: relative;
+                  "><span style="
+                      display: block;
+                      width: initial;
+                      height: initial;
+                      background: none;
+                      opacity: 1;
+                      border: 0px;
+                      margin: 0px;
+                      padding: 100% 0px 0px;
+                    "></span><img alt="logo" src="/_next/static/media/search-icon.fe541e20.svg" decoding="async"
+                  data-nimg="responsive" sizes="100vw" style="
+                      position: absolute;
+                      inset: 0px;
+
+                      padding: 0px;
+                      border: none;
+                      margin: auto;
+                      display: block;
+                      width: 0px;
+                      height: 0px;
+                      min-width: 100%;
+                      max-width: 100%;
+                      min-height: 100%;
+                      max-height: 100%;
+                    " /></span>
+            </button>
+          </div>
+        </div>
+        <span class="style_separator__n6bds d-none d-md-block ms-1 ms-md-2 ms-lg-3"></span><button
+          onclick="toggleSideMenu()" aria-label="menu" type="button"
+          class="style_navLink__aDOS0 style_iconItem__NTDWb p-0 d-none d-md-block btn btn-primary">
+          <svg width="32px" height="32px" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg">
+            <g id="menu-icon" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+              <g id="ic_search" fill-rule="nonzero">
+                <rect id="ViewBox" x="0" y="0" width="32" height="32"></rect>
+              </g>
+              <g id="Group">
+                <rect id="Rectangle" fill-opacity="0" fill="#D8D8D8" x="0" y="0" width="32" height="32"></rect>
+                <path
+                  d="M27,20.8 L25.7150442,24 L5,24 L6.28495575,20.8 L27,20.8 Z M27,14.4 L27,17.6 L5,17.6 L5,14.4 L27,14.4 Z M27,8 L25.7150442,11.2 L5,11.2 L6.28495575,8 L27,8 Z"
+                  id="Shape" fill="#FFFFFF"></path>
+              </g>
+            </g>
+          </svg>
+        </button>
+      </div>
+    </nav>
+  </div>
---
+++ resources/views/layout/header_menu.blade.php
@@ -0,0 +1,99 @@
+<div class="style_headerMenu__YNVcR xsmall-text d-flex text-nowrap">
+    <ul class="d-flex mb-0 m-auto">
+      <li><a class="style_navItem__o66ST  style_liveUpdate__l5Ln_ text-uppercase font-bold d-block" target="_self"
+          href="/live-scores/?ref=hm">Live Scores</a></li>
+      <li><a class="style_navItem__o66ST   text-uppercase font-bold d-block" target="_self"
+          href="/cricket-news/?ref=hm">Cricket News</a></li>
+      <li>
+
+        <div id="uniq648979ca4c330e9fa30f2961" class="dropdown"><button id="648979ca4c330e9fa30f2961"
+            aria-expanded="false" type="button"
+            class="style_navMore__r_nff text-uppercase font-bold d-block dropdown-toggle btn btn-link">TNPL</button>
+          <div id="648979ca4c330e9fa30f2961" x-placement="bottom-start" aria-labelledby="648979ca4c330e9fa30f2961"
+            class="style_dropdownMenu__lVfAZ dropdown-menu" data-popper-reference-hidden="false"
+            data-popper-escaped="false" data-popper-placement="bottom-start" style="position: absolute; "><a
+              class="dropdown-item" href="/t20/tnpl/?ref=hm">TNPL Home</a><a class="dropdown-item"
+              href="/t20/tnpl/fixtures/?ref=hm">TNPL Schedule 2023</a><a class="dropdown-item"
+              href="/t20/tnpl/standings/?ref=hm">TNPL Points Table 2023</a><a class="dropdown-item"
+              href="/t20/tnpl/stats/?ref=hm">TNPL Stats 2023</a><a class="dropdown-item"
+              href="/tnpl-winners-and-runners-list/?ref=hm">TNPL Winners List</a></div>
+        </div>
+      </li>
+      <li><a class="style_navItem__o66ST   text-uppercase font-bold d-block" target="_self"
+          href="/t20/lpl-auction/?ref=hm">LPL Auction</a></li>
+      <li><a class="d-none" href="/womens-emerging-teams-asia-cup/?ref=hm">W-Emerging Asia Cup Home</a><a
+          class="d-none" href="/womens-emerging-teams-asia-cup/fixtures/?ref=hm">W-Emerging Asia Cup Schedule
+          2023</a><a class="d-none" href="/womens-emerging-teams-asia-cup/standings/?ref=hm">W-Emerging Asia Cup
+          Points Table 2023</a>
+        <div id="uniq648979ca4c330e9fa30f2968" class="dropdown"><button id="648979ca4c330e9fa30f2968"
+            aria-expanded="false" type="button"
+            class="style_navMore__r_nff text-uppercase font-bold d-block dropdown-toggle btn btn-link">W-Emerging Asia
+            Cup</button>
+
+
+
+
+          <div id="648a9fda36430c395edf2678" x-placement="bottom-start" aria-labelledby="648a9fda36430c395edf2678"
+            class="style_dropdownMenu__lVfAZ dropdown-menu " data-popper-reference-hidden="false"
+            data-popper-escaped="false" data-popper-placement="bottom-start" style="position: absolute;"><a
+              class="dropdown-item" href="/womens-emerging-teams-asia-cup/?ref=hm">W-Emerging Asia Cup Home</a><a
+              class="dropdown-item" href="/womens-emerging-teams-asia-cup/fixtures/?ref=hm">W-Emerging Asia Cup
+              Schedule 2023</a><a class="dropdown-item"
+              href="/womens-emerging-teams-asia-cup/standings/?ref=hm">W-Emerging Asia Cup Points Table 2023</a></div>
+
+
+
+
+        </div>
+      </li>
+      <li>
+        <div id="uniq648979ca4c330e9fa30f296c" class="dropdown"><button id="648979ca4c330e9fa30f296c"
+            aria-expanded="false" type="button"
+            class="style_navMore__r_nff text-uppercase font-bold d-block dropdown-toggle btn btn-link">SPL</button>
+
+
+
+          <div id="648a9fda36430c395edf2671" x-placement="bottom-start"
+            class="style_dropdownMenu__lVfAZ dropdown-menu " data-popper-escaped="false"
+            data-popper-placement="bottom-start" style="position: absolute; "><a class="dropdown-item"
+              href="/t20/saurashtra-premier-league/?ref=hm">SPL Home</a><a class="dropdown-item"
+              href="/t20/saurashtra-premier-league/standings/?ref=hm">SPL Schedule 2023</a></div>
+
+
+        </div>
+      </li>
+      <li>
+        <a class="style_navItem__o66ST   text-uppercase font-bold d-block" target="_self"
+          href="/upcoming-cricket-schedule/?ref=hm">Blast</a>
+      </li>
+      <li><a class="style_navItem__o66ST   text-uppercase font-bold d-block" target="_self"
+          href="/upcoming-cricket-schedule/?ref=hm">MPL Home</a></li>
+      <li><a class="style_navItem__o66ST   text-uppercase font-bold d-block" target="_self"
+          href="/fantasy-cricket-tips/?ref=hm">Fantasy Tips</a></li>
+      <li><a class="style_navItem__o66ST   text-uppercase font-bold d-block" target="_self"
+          href="/cricket-match-predictions/?ref=hm">Predictions</a></li>
+      <li><a class="style_navItem__o66ST   text-uppercase font-bold d-block" target="_self"
+          href="/upcoming-cricket-schedule/?ref=hm">Fixtures</a></li>
+      <li>
+
+        <div id="uniq648979ca4c330e9fa30f297d" class="dropdown"><button id="648979ca4c330e9fa30f297d"
+            aria-expanded="false" type="button"
+            class="style_navMore__r_nff text-uppercase font-bold d-block dropdown-toggle btn btn-link">☰</button>
+          <div id="648a9fda36430c395edf2686" x-placement="bottom-end" aria-labelledby="648a9fda36430c395edf2686"
+            class="style_dropdownMenu__lVfAZ dropdown-menu " data-popper-reference-hidden="false"
+            data-popper-escaped="false" data-popper-placement="bottom-end"
+            style="position: absolute; right : 0; left : unset; "><a
+              class="dropdown-item" href="/t20/lpl-auction/?ref=hm">LPL Auction 2023</a><a class="dropdown-item"
+              href="/tests/world-test-championship/?ref=hm">WTC</a><a class="dropdown-item"
+              href="/world-test-championship-schedule/?ref=hm">WTC Schedule 2023-25</a><a class="dropdown-item"
+              href="/t20/ipl-indian-premier-league/?ref=hm">IPL 2023</a><a class="dropdown-item"
+              href="/cricket-analysis/?ref=hm">Analysis</a><a class="dropdown-item"
+              href="/cricket-videos/?ref=hm">Cricket Videos</a><a class="dropdown-item"
+              href="/icc-rankings/?ref=hm">ICC Ranking</a><a class="dropdown-item"
+              href="/social-tracker-cricket/?ref=hm">Social Tracker</a><a class="dropdown-item"
+              href="/twitter-reactions-cricket/?ref=hm">Twitter Reactions</a><a class="dropdown-item"
+              href="/cricket-appeal/?ref=hm">Cricket Appeal</a></div>
+        </div>
+      </li>
+    </ul>
+  </div>
